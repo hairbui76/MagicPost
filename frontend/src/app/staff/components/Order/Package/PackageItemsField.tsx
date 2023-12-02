e1e133946e26d930c89573da98e377e41a43d257
httung12303 <92967099+httung12303@users.noreply.github.com>
2023-12-02 13:26:15 +0700

    🔧 Fix responsiveness

--- frontend/src/app/staff/components/Order/Package/PackageItemsField.tsx
+++ frontend/src/app/staff/components/Order/Package/PackageItemsField.tsx
@@ -27,14 +27,14 @@ export default function PackageItemsField({
 			</PrimaryButton>
 			<hr className="border" />
 			<div className="flex flex-col gap-4 sm:flex-row sm:w-full sm:gap-44">
-				<div className="flex flex-row w-1/2">
+				<div className="flex flex-row w-full sm:w-1/2">
 					Total weight:{" "}
 					<span className="ml-auto font-medium">
 						{value.reduce((totalWeight, item) => totalWeight + item.weight, 0)}{" "}
 						g
 					</span>
 				</div>
-				<div className="flex flex-row w-1/2">
+				<div className="flex flex-row w-full sm:w-1/2">
 					Total value:{" "}
 					<span className="ml-auto font-medium">
 						{value.reduce((totalWeight, item) => totalWeight + item.value, 0)}{" "}

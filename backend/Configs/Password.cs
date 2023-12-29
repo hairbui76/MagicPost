5c7d5a865a4a63541fb5a7c7a27d63cc2867b1a7
MrYasuo <buihai0706vn@gmail.com>
2023-12-29 22:53:30 +0700

    :wrench: Fix default password

--- backend/Configs/Password.cs
+++ backend/Configs/Password.cs
@@ -4,7 +4,7 @@ namespace MagicPostApi.Configs;
 
 public class Password
 {
-	public const string DEFAULT_PASSWORD = "m@gicpost123";
+	public const string DEFAULT_PASSWORD = "magicpost";
 	public static string Hash(string password) => Argon2.Hash(password);
 
 	public static bool Verify(string hash, string password) => Argon2.Verify(hash, password);
